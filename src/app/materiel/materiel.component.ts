85460dc07f0f7e17900ce36b75c821df88930109
Costet Rayane <rayane.costet@edu.devinci.fr>
2020-05-27 16:55:50 +0200

    push materiel

--- src/app/materiel/materiel.component.ts
+++ src/app/materiel/materiel.component.ts
@@ -8,6 +8,66 @@ import {Router} from '@angular/router';
 })
 export class MaterielComponent implements OnInit {
 
+  array: any[] = [
+    {
+      url: '../../assets/tuyau.png',
+      titre: 'Tuyau'
+    },
+    {
+      url: '../../assets/pistolet.png',
+      titre: 'Pistolet'
+    },
+    {
+      url: '../../assets/tuyau1.png',
+      titre: 'Tuyau'
+    },
+    {
+      url: '../../assets/fourche.png',
+      titre: 'Fourche'
+    },
+    {
+      url: '../../assets/sécateur.png',
+      titre: 'Sécateur en Titane'
+    },
+    {
+      url: '../../assets/sécateur1.png',
+      titre: 'Sécateur'
+    },
+    {
+      url: '../../assets/rateau.png',
+      titre: 'Râteau'
+    },
+    {
+      url: '../../assets/rateau1.png',
+      titre: 'Rateau à Feuille'
+    },
+    {
+      url: '../../assets/tuyau.png',
+      titre: 'Tuyau'
+    },
+    {
+      url: '../../assets/pistolet.png',
+      titre: 'Pistolet'
+    },
+    {
+      url: '../../assets/tuyau1.png',
+      titre: 'Tuyau'
+    },
+    {
+      url: '../../assets/fourche.png',
+      titre: 'Fourche'
+    },
+    {
+      url: '../../assets/sécateur.png',
+      titre: 'Sécateur en titane'
+    },
+    {
+      url: '../../assets/sécateur1.png',
+      titre: 'Sécateur'
+    }
+  ];
+
+
   goToMateriel(){
     this.router.navigateByUrl('materiel');
   }
